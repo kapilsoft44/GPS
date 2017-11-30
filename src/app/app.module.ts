78b1fb4156eb4f265ff40f8b619a156ff00f3b9a
kapilsoft44 <kapilsoft44@gmail.com>
2017-11-30 11:59:49 +0530

    Add Login Componet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { rootRoute } from '@angular/router/src/router_module';
+
+
+const appRoutes: Routes = [{path: '', component:AppComponent},
+{path: 'login', component:LoginComponent}
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
